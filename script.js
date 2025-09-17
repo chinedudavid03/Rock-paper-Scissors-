49f873041de9caedc504286dfeed5050bf5fe48e
chinedudavid03 <chinedudavid20063@gmail.com>
2025-09-17 10:09:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -99,9 +99,9 @@ let icon = document.querySelector(".icon");
 icon.onclick = function () {
   document.body.classList.toggle("dark-mode");
 
-  if (document.body.classList.contains("dark-mode")) {
-    icon.src = "🌙";
+   if (document.body.classList.contains("dark-mode")) {
+    icon.src = "Images/dark mode.png";
   } else {
-    icon.src = "☀";
+    icon.src = "Images/light mode.png";
   }
 };
